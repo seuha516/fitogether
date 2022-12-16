ffb721866d067d99839698c04d8fe507e533fa32
SeungHa Jeon <seuha516@naver.com>
2022-12-16 16:10:30 +0900

    netlify

--- frontend/src/store/apis/client.ts
+++ frontend/src/store/apis/client.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const client = axios.create();
+const client = axios.create({
+    withCredentials: true,
+  });
+  
+client.defaults.baseURL = `${process.env.REACT_APP_API_URL}`;
 
 client.defaults.xsrfCookieName = 'csrftoken';
 client.defaults.xsrfHeaderName = 'X-CSRFToken';
